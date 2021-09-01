a57dd1d93a7e5d6bcddf65d87cb267ce444fd392
Kyouru <ren.kyouru@gmail.com>
2021-09-01 00:01:52 -0500

    connectionString Dummy

---
+++ PagosAelucoop/ConnString_dummy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagosAelucoop
+{
+    class ConnString
+    {
+        
+        public string sqlservercasa = "Data Source="; //Completar Connection String
+
+        public string GetString(string _name)
+        {
+            try
+            {
+                return (string)typeof(ConnString).GetField(_name).GetValue(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
